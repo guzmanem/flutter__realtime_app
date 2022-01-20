b5ba5075b0cf89e57fd6bf8131d9aead74babead
@eguzman2 <eguzman@buk.cl>
2022-01-20 00:22:28 -0300

    feat: conexion con firebase

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -7,18 +7,28 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: const EdgeInsets.all(8),
-          child: const Text('This works!!'),
-        ),
-      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance
+              .collection('chats/IGzJcZBneJpZZx2ZeuIe/messages')
+              .snapshots(),
+          builder: (ctx, streamSnapshot) {
+            if (streamSnapshot.connectionState == ConnectionState.waiting) {
+              return const Center(  
+                child: CircularProgressIndicator(),
+              );
+            }
+            final documents = streamSnapshot.data!.documents;
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (ctx, index) => Container(
+                padding: const EdgeInsets.all(8),
+                child: Text(documents[index]['text']),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance.collection('chats/IGzJcZBneJpZZx2ZeuIe/messages');
-        },
+        child: const Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
